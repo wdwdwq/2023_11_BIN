9d3b6c67491887008df72fd9bd93aad661e93348
wdwdwq <qkenr7278@gmail.com>
2023-11-09 19:34:57 +0900

    게시물 생성

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,33 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		
 		System.out.println("==프로그램 시작==");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		
-		while(true) {
+		int articleCount = 0;
+
+		while (true) {
 			System.out.println("명령어)");
 			String cmd = sc.nextLine().trim();
-			
-			if(cmd.length() == 0) {
-				System.out.println("명령어를 입력해수제요");
-				continue;
-			}
-			
-			if(cmd.equals("exit")) {
+
+			if (cmd.equals("exit")) {
 				break;
+			} else if (cmd.equals("article write")) {
+				System.out.println("제목: ");
+				String title = sc.nextLine();
+				System.out.println("내용: ");
+				String content = sc.nextLine();
+				articleCount++;
+				System.out.println(articleCount + "번 게시물이 생성되었습니다");
+
+			} else {
+				System.out.println("올바른 명령어를 입력해주세요");
 			}
-			
+
 		}
 
-		
 		System.out.println("==프로그램 끝==");
-		
+
 		sc.close();
 	}
 
